ae6356aaaf1db72ec08b4995469a9a4bd1f2ffad
Samuel de Framond <sdeframond@kozea.fr>
2010-03-11 17:22:47 +0100

    Add mocha to dependencies

--- sources/config/environment.rb
+++ sources/config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "rspec"
   config.gem "authlogic"
+  config.gem "mocha"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
