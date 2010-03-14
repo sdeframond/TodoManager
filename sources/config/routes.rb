8711cef01812972b706eb5ebba5082765890efa9
Samuel de Framond <sdeframond@kozea.fr>
2010-03-15 00:30:26 +0100

    Add nested resources for todos

--- sources/config/routes.rb
+++ sources/config/routes.rb
@@ -1,7 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :user_sessions
 
-  map.resources :users
+  map.resources :users, :shallow => true do |users|
+    users.resources :todos, :controller => :todo_items
+  end
   map.resource :password_reset
 
   # The priority is based upon order of creation: first created -> highest priority.
